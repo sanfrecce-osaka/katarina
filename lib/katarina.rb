33923e0097ddaf53858e0228576d4467b9721169
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-08-01 07:22:55 +0900

    fix katarina.rb

--- lib/katarina.rb
+++ lib/katarina.rb
@@ -1,6 +1,21 @@
-require "katarina/version"
+# frozen_string_literal: true
+
+require 'ruby-next'
+require 'ruby-next/language/setup'
+RubyNext::Language.setup_gem_load_path(transpile: true)
+
+require 'katarina/version'
+require 'katarina/config'
+require 'katarina/path'
+require 'katarina/schema'
+require 'katarina/parser'
+require 'katarina/printer'
+require 'katarina/type_generator'
+require 'katarina/generatable'
+require 'katarina/configurable'
+require 'katarina/railtie' if defined?(Rails)
 
 module Katarina
-  class Error < StandardError; end
-  # Your code goes here...
+  extend Generatable
+  extend Configurable
 end
