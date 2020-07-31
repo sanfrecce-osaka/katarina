7f1ec540dbcb13d02448577da50b5be8ae80dfac
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2020-08-01 07:17:19 +0900

    add parser.rb
    - Due to the constraints of pattern matching, path_and_method_pairs is used to create an array of path and http_method pairs before creating a Schema object for each response code.
    - If Numbered Parameter was used more than twice in a method chain, it was not transpiled to block arguments after the second time. That's why I also use Method#to_proc.

---
+++ lib/katarina/parser.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'yaml'
+
+module Katarina
+  module Parser
+    class << self
+      def parse(path)
+        path
+          .then(&File.method(:read))
+          .then { YAML.load(_1, symbolize_names: true) }
+          .then(&method(:build_schemas))
+      end
+
+      private
+
+      def build_schemas(doc)
+        path_and_method_pairs(doc[:paths]).flat_map do |pair|
+          pair in { path:, http_method: }
+          doc[:paths][path][http_method] in { responses:, summary: }
+          responses.keys.map do |response_code|
+            Schema.new(
+              path,
+              *summary.split(' ').then { [_1.first, _1.last[1..]] },
+              http_method,
+              response_code,
+              responses[response_code][:content][:'application/json'][:schema]
+            )
+          end
+        end
+      end
+
+      def path_and_method_pairs(paths)
+        paths.keys.flat_map do |path|
+          paths[path].keys.map do |http_method|
+            { path: path, http_method: http_method }
+          end
+        end
+      end
+    end
+  end
+end
